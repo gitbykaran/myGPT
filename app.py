50082583d6f902c63e1354d0b1b559c346fdb8fa
karanongithub <karandeepsingh3451@gmail.com>
2024-04-18 10:28:58 +0530

    changes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from langchain.chains import SequentialChain
 from langchain.prompts import PromptTemplate
-from langchain.llms import OPENAI
+from langchain.llms import OpenAI
 from apikey import key
 import os
 
 os.environ['OPENAI_API_KEY'] = key
 
 
-llm = OPENAI(temperature=0.9)
+llm = OpenAI(temperature=0.9)
 
 st.title('MY GPT:')
 prompt = st.text_input('Enter your prompt here')
